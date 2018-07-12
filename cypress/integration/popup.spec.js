e9de17a7721cbbf2e8d380d72be05f6752619331
Nelson Omuto <nelsonomuto@gmail.com>
2018-07-11 23:06:58 -0700

    - adding lint-staged, cypress, jest, selenium
    update deps
    
    ensure unit test config is updated
    
    add lint-staged and configure
    
    update ignore

---
+++ cypress/integration/popup.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('hasthelargehadroncolliderdestroyedtheworldyet', () => {
+  beforeEach(() => {
+    cy.visit('http://hasthelargehadroncolliderdestroyedtheworldyet.com/')
+  })
+
+  it('should load save to pocket extension', () => {
+
+  })
+})
